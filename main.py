c799a49d99d3e2b9ee951400c601eeb6e03b350d
eryzhkova <esryzhkova.dev@gmail.com>
2021-05-31 20:13:03 +0500

    add proxy selenium wire

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-import random
-import time
-import random
-from selenium import webdriver
+from auth_data import CHROMEDRIVER_PATH, PROXY
 from fake_useragent import UserAgent
+from seleniumwire import webdriver
+import requests
+import json
+import time
 
-url = "https://www.avito.ru/perm"
-useragent = UserAgent()
+url = "https://www.vk.com/"
+
 # options
+user_agent = UserAgent()
 options = webdriver.ChromeOptions()
-options.add_argument(f"user-agent={useragent.random}")
+options.add_argument(f"user-agent={user_agent.random}")
+
+# set proxy
+proxy_options = {
+    "proxy": {
+        "https": f"https://{PROXY}"
+    }
+}
 
 driver = webdriver.Chrome(
-    executable_path="C:\\Users\\fryje\\PycharmProjects\\psuParserTelegram\\chromedriver.exe",
+    executable_path=CHROMEDRIVER_PATH,
+    seleniumwire_options=proxy_options,
     options=options)
 
 try:
+    # driver.get("https://2ip.ru")
+    # driver.get("https://www.whatismybrowser.com/detect/what-is-my-user-agent")
     driver.get(url)
-    time.sleep(5)
+    time.sleep(20)
 except Exception as ex:
     print(ex)
 finally:
