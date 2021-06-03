e9c396ed7b874e5b71c7383c65841e90452de5c5
eryzhkova <esryzhkova.dev@gmail.com>
2021-06-03 14:55:31 +0500

    Add proxy and new options

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 from auth_data import CHROMEDRIVER_PATH, PROXY
 from fake_useragent import UserAgent
 from seleniumwire import webdriver
-import requests
-import json
 import time
 
-url = "https://www.vk.com/"
 
-# options
-user_agent = UserAgent()
-options = webdriver.ChromeOptions()
-options.add_argument(f"user-agent={user_agent.random}")
+def main(url):
+    # options
+    user_agent = UserAgent()
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-agent={user_agent.random}")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    # options.add_argument("--headless")
 
-# set proxy
-proxy_options = {
-    "proxy": {
-        "https": f"https://{PROXY}"
+    # set proxy
+    proxy_options = {
+        "proxy": {
+            "https": f"https://{PROXY}"
+        }
     }
-}
 
-driver = webdriver.Chrome(
-    executable_path=CHROMEDRIVER_PATH,
-    seleniumwire_options=proxy_options,
-    options=options)
+    driver = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH,
+        seleniumwire_options=proxy_options,
+        options=options)
 
-try:
-    # driver.get("https://2ip.ru")
-    # driver.get("https://www.whatismybrowser.com/detect/what-is-my-user-agent")
-    driver.get(url)
-    time.sleep(20)
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+    try:
+        # driver.get("https://2ip.ru")
+        # driver.get("https://www.whatismybrowser.com/detect/what-is-my-user-agent")
+        driver.get(url)
+        time.sleep(20)
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
+
+
+if __name__ == '__main__':
+    url = "https://www.vk.com/"
+    main(url)
