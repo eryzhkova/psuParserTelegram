73dbcb73913a6544b2063768f3f0eca0852f9a70
Elena Ryzhkova <79700230+eryzhkova@users.noreply.github.com>
2021-06-18 21:33:31 +0500

    Work with JSON and Bot
    * try waiting user messages
    
    * new staff
    
    * finish work with bot and json

--- keyboard.py
+++ keyboard.py
@@ -3,22 +3,24 @@ from telebot import types
 
 class TelegramKeyboards:
 
-    def __init__(self, sites, city):
-        self.sites = sites
-        self.city = city
-
-    def main_setting_keyboard(self):
+    def main_setting_keyboard(self, user, id):
         keyboard = types.InlineKeyboardMarkup()
+        sites = len(user[f'{id}']["sites"])
+        city = user[f'{id}']["city"]
 
-        realty_site = types.InlineKeyboardButton(text=f'Сайт объявлений [Выбрано: {self.sites}]', callback_data='sites')
+        realty_site = types.InlineKeyboardButton(text=f'Сайт объявлений [Выбрано: {sites}]', callback_data='sites')
         filters = types.InlineKeyboardButton(text='Критерии поиска', callback_data='filters')
-        location = types.InlineKeyboardButton(text=f'Местоположение [{self.city}]', callback_data='location')
+        if city is None:
+            location = types.InlineKeyboardButton(text='Местоположение [Не выбрано]', callback_data='location')
+        else:
+            location = types.InlineKeyboardButton(text=f'Местоположение [{city}]', callback_data='location')
+        view = types.InlineKeyboardButton(text=f'Просмотр', callback_data='view')
         confirmation = types.InlineKeyboardButton(text='Подтвердить', callback_data='confirmation')
 
         keyboard.add(realty_site)
         keyboard.add(filters)
         keyboard.add(location)
-        keyboard.add(confirmation)
+        keyboard.add(view, confirmation)
         return keyboard
 
     def sites_setting_keyboard(self):
@@ -33,11 +35,23 @@ class TelegramKeyboards:
         keyboard.add(ok_btn)
         return keyboard
 
+    def checked_sites_setting_keyboard(self, text_avito, text_cian, text_domofond):
+        keyboard = types.InlineKeyboardMarkup()
+        avito = types.InlineKeyboardButton(text=f'{text_avito}Авито', callback_data='avito')
+        cian = types.InlineKeyboardButton(text=f'{text_cian}Циан', callback_data='cian')
+        domofond = types.InlineKeyboardButton(text=f'{text_domofond}Домофонд', callback_data='domofond')
+        ok_btn = types.InlineKeyboardButton(text=f'ОК', callback_data='okbtn')
+        del_btn = types.InlineKeyboardButton(text=f'Убрать все', callback_data='delbtn')
+
+        keyboard.add(avito, cian, domofond)
+        keyboard.add(ok_btn, del_btn)
+        return keyboard
+
     def location_setting_keyboard(self):
         keyboard = types.InlineKeyboardMarkup()
 
         perm = types.InlineKeyboardButton(text=f'Пермь', callback_data='perm')
-        lisva = types.InlineKeyboardButton(text='Лысьва', callback_data='lisva')
+        lisva = types.InlineKeyboardButton(text='Лысьва', callback_data='lysva')
         permkrai = types.InlineKeyboardButton(text=f'Пермский край', callback_data='permkrai')
         dobryanka = types.InlineKeyboardButton(text=f'Добрянка', callback_data='dobryanka')
         ok_btn = types.InlineKeyboardButton(text=f'ОК', callback_data='okbtn')
@@ -45,6 +59,20 @@ class TelegramKeyboards:
         keyboard.add(perm, lisva, dobryanka)
         keyboard.add(permkrai)
         keyboard.add(ok_btn)
+        return keyboard
+
+    def checked_location_setting_keyboard(self, text_perm, text_lysva, text_permkrai, text_dobryanka):
+        keyboard = types.InlineKeyboardMarkup()
+        perm = types.InlineKeyboardButton(text=f'{text_perm}Пермь', callback_data='perm')
+        lisva = types.InlineKeyboardButton(text=f'{text_lysva}Лысьва', callback_data='lysva')
+        permkrai = types.InlineKeyboardButton(text=f'{text_permkrai}Пермский край', callback_data='permkrai')
+        dobryanka = types.InlineKeyboardButton(text=f'{text_dobryanka}Добрянка', callback_data='dobryanka')
+        ok_btn = types.InlineKeyboardButton(text=f'ОК', callback_data='okbtn')
+        del_btn = types.InlineKeyboardButton(text=f'Очистить', callback_data='clearbtn')
+
+        keyboard.add(perm, lisva, dobryanka)
+        keyboard.add(permkrai)
+        keyboard.add(ok_btn, del_btn)
         return keyboard
 
     def house_setting_replykeyboard(self):
@@ -61,8 +89,8 @@ class TelegramKeyboards:
     def ad_setting_replykeyboard(self):
         keyboard = types.ReplyKeyboardMarkup()
 
-        sell_ad = types.KeyboardButton(text='Продажа')
-        rent_ad = types.KeyboardButton(text='Аренда')
+        sell_ad = types.KeyboardButton(text='Купить')
+        rent_ad = types.KeyboardButton(text='Снимать')
 
         keyboard.row(sell_ad, rent_ad)
         return keyboard
@@ -83,7 +111,7 @@ class TelegramKeyboards:
 
     def rent_min_setting_replykeyboard(self):
         keyboard = types.ReplyKeyboardMarkup()
-        min = types.KeyboardButton(text='0')
+        min = types.KeyboardButton(text='Нет')
         min10 = types.KeyboardButton(text='10000')
         keyboard.row(min, min10)
         return keyboard
@@ -91,7 +119,7 @@ class TelegramKeyboards:
     def sell_min_setting_replykeyboard(self):
         keyboard = types.ReplyKeyboardMarkup()
 
-        min = types.KeyboardButton(text='0')
+        min = types.KeyboardButton(text='Нет')
         min1 = types.KeyboardButton(text='1000000')
 
         keyboard.row(min, min1)
